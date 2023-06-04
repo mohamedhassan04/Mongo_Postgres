332f439988d40e93e8758f703be8ab1c17936778
mohamed <hassenmohamed.92@gmail.com>
2023-06-04 16:15:39 +0100

    config docker and the 2 database

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,14 @@
+// user.entity.ts (Postgres)
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
